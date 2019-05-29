23c17684e8b3b856d23c9b46f3c8b299a5a65a29
surixon <surixon123@gmail.com>
2019-05-29 11:19:54 +0530

    commiting new

---
+++ platform_www/plugins/cordova-plugin-android-fingerprint-auth/www/FingerprintAuth.js
@@ -0,0 +1,29 @@
+cordova.define("cordova-plugin-android-fingerprint-auth.FingerprintAuth", function(require, exports, module) {
+function FingerprintAuth() {
+}
+
+FingerprintAuth.prototype.show = function (params, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        "FingerprintAuth",  // Java Class
+        "authenticate", // action
+        [ // Array of arguments to pass to the Java class
+            params
+        ]
+    );
+};
+
+FingerprintAuth.prototype.isAvailable = function (successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        "FingerprintAuth",  // Java Class
+        "availability", // action
+        [{}]
+    );
+};
+
+FingerprintAuth = new FingerprintAuth();
+module.exports = FingerprintAuth;
+});
